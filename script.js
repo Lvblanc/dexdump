32adf532ea256d4599f17d704fb4877684d6ecf1
Lvblanc <75372156+Lvblanc@users.noreply.github.com>
2023-06-21 09:53:36 +0700

    script.js

--- script.js
+++ script.js
@@ -1,125 +1,95 @@
-function get_self_process_name() {
-    var openPtr = Module.getExportByName('libc.so', 'open');
-    var open = new NativeFunction(openPtr, 'int', ['pointer', 'int']);
+const fork_ptr = Module.getExportByName(null, "fork");
+const fork = new NativeFunction(fork_ptr, 'int', []);
+Interceptor.replace(fork_ptr, new NativeCallback(function() {
+    console.warn("Fork Found and Replaced");
+    //return fork()
+    return -1;
+}, "int", []));
 
-    var readPtr = Module.getExportByName("libc.so", "read");
-    var read = new NativeFunction(readPtr, "int", ["int", "pointer", "int"]);
-
-    var closePtr = Module.getExportByName('libc.so', 'close');
-    var close = new NativeFunction(closePtr, 'int', ['int']);
-
-    var path = Memory.allocUtf8String("/proc/self/cmdline");
-    var fd = open(path, 0);
+function ProcessName() {
+    let openPtr = Module.getExportByName('libc.so', 'open');
+    let open = new NativeFunction(openPtr, 'int', ['pointer', 'int']);
+    let readPtr = Module.getExportByName('libc.so', 'read');
+    let read = new NativeFunction(readPtr, 'int', ['int', 'pointer', 'int']);
+    let closePtr = Module.getExportByName('libc.so', 'close');
+    let close = new NativeFunction(closePtr, 'int', ['int']);
+    let path = Memory.allocUtf8String('/proc/self/cmdline');
+    let fd = open(path, 0);
     if (fd != -1) {
-        var buffer = Memory.alloc(0x1000);
-
-        var result = read(fd, buffer, 0x1000);
+        let buffer = Memory.alloc(0x1000);
+        let result = read(fd, buffer, 0x1000);
         close(fd);
         result = ptr(buffer).readCString();
         return result;
     }
-
-    return "-1";
+    return -1;
 }
 
-function dump_dex() {
-    var libart = Process.findModuleByName("libart.so");
-    var addr_DefineClass = null;
-    var symbols = libart.enumerateSymbols();
-    for (var index = 0; index < symbols.length; index++) {
-        var symbol = symbols[index];
-        var symbol_name = symbol.name;
-        //这个DefineClass的函数签名是Android9的
-        //_ZN3art11ClassLinker11DefineClassEPNS_6ThreadEPKcmNS_6HandleINS_6mirror11ClassLoaderEEERKNS_7DexFileERKNS9_8ClassDefE
-        if (symbol_name.indexOf("ClassLinker") >= 0 && 
-            symbol_name.indexOf("DefineClass") >= 0 && 
-            symbol_name.indexOf("Thread") >= 0 && 
-            symbol_name.indexOf("DexFile") >= 0 ) {
-            console.log(symbol_name, symbol.address);
+function ProcessDex(Buf, C,Path) {
+    let lvblanc = new Uint8Array(Buf);
+    let Count = C - 1;
+    if (lvblanc[0] == 99 && lvblanc[1] == 100 && lvblanc[2] == 101 && lvblanc[3] == 120 && lvblanc[4] == 48 && lvblanc[5] == 48 && lvblanc[6] == 49) {
+        console.warn("[*]  classes" + Count + ".dex is CDex. Ignore It.");       
+    } else
+    if (lvblanc[0] == 0 && lvblanc[1] == 0 && lvblanc[2] == 0 && lvblanc[3] == 0 && lvblanc[4] == 0 && lvblanc[5] == 0 && lvblanc[6] == 0) {
+        console.warn("[*] 0000000 Header. Probably classes" + Count + ".dex is Dexprotector's Dex.");
+        console.error("[Dex"+Count +"] : "+Path);
+        WriteDex(Count,Buf,Path,0);        
+    } else
+    if (lvblanc[0] == 0 || lvblanc[0] != 100) {
+        console.warn("[*] Wiped Header , classes" + Count + ".dex is Interesting Dex.");
+        console.error("[Dex"+Count +"] : "+Path);      
+        WriteDex(Count,Buf,Path,0);
+    } else {
+        WriteDex(Count,Buf,Path,1);
+    }
+}
+function WriteDex(Count,Buffer,Path,Flag) {   
+   let DexFD = new File(Path, "wb");
+   DexFD.write(Buffer)
+   DexFD.flush();
+   DexFD.close();
+   if(Flag == 0){
+      console.warn("[Dex"+Count +"] : "+Path);
+   } else {
+     console.log("[Dex"+Count +"] : "+Path);
+   }
+}
+
+function Dump_Dex() {
+    let Pro = ProcessName();
+    let libart = Process.findModuleByName("libart.so");
+    let addr_DefineClass = null;
+    let symbols = libart.enumerateSymbols();
+    for (let index = 0; index < symbols.length; index++) {
+        let symbol = symbols[index];
+        let symbol_name = symbol.name;
+        if (symbol_name.indexOf("ClassLinker") >= 0 && symbol_name.indexOf("DefineClass") >= 0 && symbol_name.indexOf("Thread") >= 0 && symbol_name.indexOf("DexFile") >= 0) {
             addr_DefineClass = symbol.address;
+            console.log("Dumping is running");
         }
     }
-    var dex_maps = {};
-
-    console.log("[DefineClass:]", addr_DefineClass);
+    let dex_maps = {};
+    let dex_count = 1;
     if (addr_DefineClass) {
         Interceptor.attach(addr_DefineClass, {
-            onEnter: function (args) {
-                var dex_file = args[5];
-                //ptr(dex_file).add(Process.pointerSize) is "const uint8_t* const begin_;"
-                //ptr(dex_file).add(Process.pointerSize + Process.pointerSize) is "const size_t size_;"
-                var base = ptr(dex_file).add(Process.pointerSize).readPointer();
-                var size = ptr(dex_file).add(Process.pointerSize + Process.pointerSize).readUInt();
-
+            onEnter: function(args) {
+                let dex_file = args[5];
+                let base = ptr(dex_file).add(Process.pointerSize).readPointer();
+                let size = ptr(dex_file).add(Process.pointerSize + Process.pointerSize).readUInt();
                 if (dex_maps[base] == undefined) {
                     dex_maps[base] = size;
-                    var magic = ptr(base).readCString();
-                    if (magic.indexOf("dex") == 0) {
-                        var process_name = get_self_process_name();
-                        if (process_name != "-1") {
-                            var dex_path = "/data/data/" + process_name + "/files/" + base.toString(16) + "_" + size.toString(16) + ".dex";
-                            console.log("[find dex]:", dex_path);
-                            var fd = new File(dex_path, "wb");
-                            if (fd && fd != null) {
-                                var dex_buffer = ptr(base).readByteArray(size);
-                                fd.write(dex_buffer);
-                                fd.flush();
-                                fd.close();
-                                console.log("[dump dex]:", dex_path);
-
-                            }
-                        }
-                    }
+                    let dex_dir_path = "/data/data/" + Pro + "/";                                                        
+                    let dex_path = dex_dir_path + "classes" + dex_count + ".dex";   
+                    dex_count++;
+                    let count_dex = dex_count;
+                    let count = count_dex -1;
+                    let dex_buffer = ptr(base).readByteArray(size);
+                    ProcessDex(dex_buffer, dex_count,dex_path);
                 }
-            }, onLeave: function (retval) {
-            }
+            },
+            onLeave: function(retval) {}
         });
     }
 }
-
-var is_hook_libart = false;
-
-function hook_dlopen() {
-    Interceptor.attach(Module.findExportByName(null, "dlopen"), {
-        onEnter: function (args) {
-            var pathptr = args[0];
-            if (pathptr !== undefined && pathptr != null) {
-                var path = ptr(pathptr).readCString();
-                //console.log("dlopen:", path);
-                if (path.indexOf("libart.so") >= 0) {
-                    this.can_hook_libart = true;
-                    console.log("[dlopen:]", path);
-                }
-            }
-        },
-        onLeave: function (retval) {
-            if (this.can_hook_libart && !is_hook_libart) {
-                dump_dex();
-                is_hook_libart = true;
-            }
-        }
-    })
-
-    Interceptor.attach(Module.findExportByName(null, "android_dlopen_ext"), {
-        onEnter: function (args) {
-            var pathptr = args[0];
-            if (pathptr !== undefined && pathptr != null) {
-                var path = ptr(pathptr).readCString();
-                //console.log("android_dlopen_ext:", path);
-                if (path.indexOf("libart.so") >= 0) {
-                    this.can_hook_libart = true;
-                    console.log("[android_dlopen_ext:]", path);
-                }
-            }
-        },
-        onLeave: function (retval) {
-            if (this.can_hook_libart && !is_hook_libart) {
-                dump_dex();
-                is_hook_libart = true;
-            }
-        }
-    });
-}
-
-
-setImmediate(dump_dex);
+setImmediate(Dump_Dex);
